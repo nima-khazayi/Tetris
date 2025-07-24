8708bf0330fffff5fe8048c3bee863698f1bffb0
Nima <Nimakhazayi7@gmail.com>
2025-07-24 19:35:26 +0330

    Update root.c

--- init/root.c
+++ init/root.c
@@ -1,18 +1,14 @@
+#include <stdlib.h>
 #include "raylib.h"
+#include "menu.h"
 
+void initializer(Texture2D stars[4]) {
 
-int initializer() {
+    // Menu Screen!        
+    float posY = 0;
+    int magik = GetRandomValue(0, 3);
+    int posX = GetRandomValue(0, 1800);
+    Vector2 ImagePos = {posX, posY};
 
-    InitWindow(800, 600, "TeTRo");
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-        EndDrawing();
-    }
-
-    CloseWindow();
-
-    return 0;
+    DrawTextureEx(stars[magik], ImagePos, 0, .01f, WHITE);
 }
