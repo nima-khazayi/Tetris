5b0cc773d7bf6193bcd8fe173ae3f9d7c2e0077c
Nima <Nimakhazayi7@gmail.com>
2025-07-24 19:34:46 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
 #include <math.h>
 #include "init/root.h"
-
+#include "raylib.h"
 
 
 int main() {
     
-    initializer();
+    InitWindow(1800, 900, "TeTRo");
+    Texture2D star_1;
+    Texture2D star_2;
+    Texture2D star_3;
+    Texture2D star_4;
+    star_1 = LoadTexture("templates/1.png");
+    star_2 = LoadTexture("templates/2.png");
+    star_3 = LoadTexture("templates/3.png");
+    star_4 = LoadTexture("templates/4.png");
+    Texture2D stars[4] = {star_1, star_2, star_3, star_4};
+
+    // Main Game Loop!
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+        initializer(stars);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
