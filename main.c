f2cff71fef106e73a5585605afd892b55c3dd50b
Nima <Nimakhazayi7@gmail.com>
2025-07-25 12:48:53 +0330

    Update main.c

--- main.c
+++ main.c
@@ -1,31 +1,87 @@
 #include <stdio.h>
-#include <math.h>
-#include "init/root.h"
 #include "raylib.h"
+#include "init/menu.h"
 
+#define STAR_COUNT 300
+
+typedef struct {
+    float x;
+    float y;
+    float speed;
+    bool active;
+    Texture2D *texture;  // Pointer to one of the 4 star textures
+
+} Star;
+
+
+Star stars[STAR_COUNT];
+int screenWidth = 1800;
+int screenHeight = 900;
+
+int activeStars = 0;
+float spawnTimer = 0.0f;
+float spawnInterval = 0.02f; // seconds between spawns (adjust for faster/slower)
+
+Texture2D starTextures[4];  // Array of star images
 
 int main() {
     
-    InitWindow(1800, 900, "TeTRo");
-    Texture2D star_1;
-    Texture2D star_2;
-    Texture2D star_3;
-    Texture2D star_4;
-    star_1 = LoadTexture("templates/1.png");
-    star_2 = LoadTexture("templates/2.png");
-    star_3 = LoadTexture("templates/3.png");
-    star_4 = LoadTexture("templates/4.png");
-    Texture2D stars[4] = {star_1, star_2, star_3, star_4};
+    InitWindow(screenWidth, screenHeight, "TeTRo");
+
+    starTextures[0] = LoadTexture("templates/1.png");
+    starTextures[1] = LoadTexture("templates/2.png");
+    starTextures[2] = LoadTexture("templates/3.png");
+    starTextures[3] = LoadTexture("templates/4.png");
+
+    for (int i = 0; i < STAR_COUNT; i++) {
+        stars[i].active = false;
+    }
 
     // Main Game Loop!
     while (!WindowShouldClose())
     {
+
+        float deltaTime = GetFrameTime();
+        spawnTimer += deltaTime;
+
+        // Spawn logic
+        if (spawnTimer >= spawnInterval && activeStars < STAR_COUNT) {
+            spawnTimer = 0.0f;
+
+            stars[activeStars].x = GetRandomValue(0, GetScreenWidth() - 32);  // assuming 32px texture width
+            stars[activeStars].y = 0;
+            stars[activeStars].speed = GetRandomValue(50, 150);
+            stars[activeStars].texture = &starTextures[GetRandomValue(0, 3)];
+            stars[activeStars].active = true;
+
+            activeStars++;
+        }
+
         BeginDrawing();
 
         ClearBackground(BLACK);
-        initializer(stars);
+        for (int i = 0; i < activeStars; i++) {
+            if (stars[i].active) {
+                stars[i].y += stars[i].speed * deltaTime;
+
+                if (stars[i].y > GetScreenHeight()) {
+                    stars[i].y = 0;
+                    stars[i].x = GetRandomValue(0, GetScreenWidth() - 32);
+                }
+
+                DrawTextureEx(*stars[i].texture, (Vector2){(int)stars[i].x, (int)stars[i].y}, 0.0f, 0.01f, WHITE);
+            }
+        }
+
+        if (activeStars >= STAR_COUNT) {
+            initializer();
+        }
 
         EndDrawing();
+    }
+
+    for (int i = 0; i < 4; i++) {
+        UnloadTexture(starTextures[i]);
     }
 
     CloseWindow();
