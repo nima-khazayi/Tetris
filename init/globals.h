4418fd01a12df5ed7808cb6ccb96240ce9155047
Nima <Nimakhazayi7@gmail.com>
2025-07-27 18:06:05 +0330

    Update globals.h

--- init/globals.h
+++ init/globals.h
@@ -4,6 +4,25 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
-extern bool shouldExit;
+#include <stdbool.h>
+
+// Enum for menu state
+typedef enum {
+    MAIN_MENU,
+    LOGIN_SCREEN,
+    GAME
+} MenuState;
+
+// Declare global variables
+extern MenuState currentMenuState;
+extern bool soundOn;
+extern bool mode;
+extern int shouldExit;
+extern bool startGame;
+extern bool block;
+extern const int numRows;
+extern const int numCols;
+extern const int cellWidth;
+extern const int cellHeight;
 
 #endif
