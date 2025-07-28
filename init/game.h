54afbd48a89001616c5577a82c27b7ab5b6dd5ae
Nima <Nimakhazayi7@gmail.com>
2025-07-28 12:56:29 +0330

    Update game.h

--- init/game.h
+++ init/game.h
@@ -1,9 +1,25 @@
-
-// Start game
-
 #ifndef GAME_H
 #define GAME_H
 
-int game_loop();
+#include "raylib.h"
 
-#endif
+// Constants
+#define BLOCK_TYPES 7
+#define BLOCK_SIZE 4
+
+// Allocate a 2D matrix of Color
+Color** allocate_matrix(int Rows, int Cols);
+
+// Free a 2D matrix of Color
+void free_matrix(Color** matrix, int Rows);
+
+// Generate all Tetris blocks
+Color*** create_all_blocks(void);
+
+// Draw a block at a grid position
+void draw_block(Color** block, int gridX, int gridY, int cellWidth, int cellHeight);
+
+// Game grid setup (placeholder)
+int grid(void);
+
+#endif // GAME_H
